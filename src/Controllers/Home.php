7bb1cfc8fec27470c0eef84ee216fe374342f33c
Henry <henryjvp24@hotmail.com>
2022-10-07 16:13:48 -0500

    Agregamos el contructor y las funciones CRUD

---
+++ src/Controllers/Home.php
@@ -0,0 +1,26 @@
+<?php
+
+    namespace Controllers;
+
+    class Home{
+
+        public function __construct(){
+            
+        }
+
+        public function Create(){
+
+        }
+
+        public function Read(){
+
+        }
+
+        public function Update(){
+
+        }
+
+        public function Delete(){
+            
+        }
+    }
