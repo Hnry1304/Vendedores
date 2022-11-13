cbdbdb8d5c9a39a9b85a0c9e38803bf211b3b1fc
henry villarreal <henryjvp24@hotmail.com>
2022-11-13 07:02:11 -0600

    Agregamos mas  tablas que usaremos en el futurogit status!

--- Tablas.sql
+++ Tablas.sql
@@ -1,10 +1,42 @@
-CREATE TABLE ventas{
+CREATE TABLE temporal(
     id  INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(10),
     producto VARCHAR(20),
     cantidad INTEGER,
     ganancia INTEGER,
-    totalGanancia FLOAT,
-    cantidadTotal INT
-}
+    Periodo VARCHAR(15)
+);
 
+CREATE TABLE vendedores(
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(10),
+    producto VARCHAR(20),
+    cantidad INTEGER,
+    ganancia INTEGER,
+    totalGanancia FLOAT,    
+    Periodo VARCHAR(15)
+);
+
+CREATE TABLE cantidadTotal(
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(10),
+    producto VARCHAR(20),
+    cantidad INTEGER,
+    Periodo VARCHAR(15)
+);
+
+CREATE TABLE gananciaTotal(
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(10),
+    producto VARCHAR(20),
+    cantidad INTEGER,
+    Periodo VARCHAR(15)
+);
+
+CREATE TABLE vendedores(
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(10),
+    producto VARCHAR(20),
+    cantidad INTEGER,
+    ganancia INTEGER    
+);
