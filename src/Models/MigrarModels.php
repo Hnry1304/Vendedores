3a8b4c87d6a900517d3d69b714c030e259ba7695
henry villarreal <henryjvp24@hotmail.com>
2022-11-13 07:39:35 -0600

    inicializamos el constructor con la conexion de base de datos, creamos la funcion migrar, tomaremos los datos de una tabla y la pasaremos a otra tabla llamada vendedores

---
+++ src/Models/MigrarModels.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace Controllers\Models;
+
+    use Controllers\Models\Datos;
+    use Controllers\Config\database;
+
+    class MigrarModels{
+
+
+        public function __construct(){
+
+            $this->conexion = new database;
+            $this->conexion = $this->conexion->connect();            
+        }
+
+        public function migrar(){
+            $sql = "SELECT *FROM ventas";
+            $sql1 = "INSERT INTO vendedores VALUES(
+                NULL,:name,:producto,:cantidad,:ganancia
+            )";
+
+
+            $stament = $this->conexion->prepare($sql);
+            $stament->execute();
+
+            $data = $stament->fetchAll();
+
+
+            foreach($data as $datas){                
+                
+                $stament = $this->conexion->prepare($sql1);
+                $stament->execute(array(
+                    ":name" => $datas['nombre'],
+                    ":producto" => $datas['producto'],
+                    ":cantidad" => $datas['cantidad'],
+                    ":ganancia" => $datas['ganancia']
+                ));
+            }
+        }
+    }
