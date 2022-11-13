acf1c66b8177c15468afa114d97912f86828c57f
henry villarreal <henryjvp24@hotmail.com>
2022-11-13 07:41:18 -0600

    inicializamos con la conexion de base de datos, la funcion readTable leera los datos extraidos de la tabla ventas

---
+++ src/Models/VendedoresModels.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace Controllers\Models;
+
+    use Controllers\Config\database;
+
+    class VendedoresModels{
+
+        public function __construct(){
+            $this->conexion = new database;
+            $this->conexion = $this->conexion->connect();
+        }
+
+
+        public function readTable($nombre){
+
+            $sql = "SELECT *FROM ventas WHERE nombre = :name";
+
+            $statement = $this->conexion->prepare($sql);
+
+            $statement->execute(array(
+                ":name" => $nombre
+            ));
+
+            $data = $statement->fetchAll();
+
+            return $data;
+        }
+        
+    }
